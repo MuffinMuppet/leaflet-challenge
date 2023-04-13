372f6eac605f553e9b23183961a22de7aea3a4bc
MuffinMuppet <you@example.com>
2023-04-13 10:17:48 -0400

    Changed Data set and added legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -37,7 +37,64 @@ function getColor(depth) {
 // Set URL of choice as Query and have it retrieve all the earthquake data in the GeoJSON format inn this URL.
 // URL is set to all week data on quakes.
 
-var query = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+var markers = L.featureGroup(); // Create a feature group to hold the markers
+
+// Set URL to variable Query and have it retrieve all the earthquake data in the GeoJSON format in this URL.
+
+var query = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
 d3.json(query).then(function(data) {
-    
-})
+    // Loop through the earthquake data and create circle markers with color and size based on magnitude and depth.
+    data.features.forEach(function(feature) {
+        var latitude = feature.geometry.coordinates[1];
+        var longitude = feature.geometry.coordinates[0];
+        var magnitude = feature.properties.mag;
+        var depth = feature.geometry.coordinates[2];
+        var time = new Date(feature.properties.time).toLocaleString();
+        var size = magnitude * 7;
+        var colorofdot = getColor(depth);
+
+        // Next make the popup markers
+        var popup = L.circleMarker([latitude, longitude], {
+            radius: size,
+            color: colorofdot,
+            fillOpacity: 0.5
+        }).bindPopup(`<h3>${feature.properties.title}</h3>
+                      <hr>
+                      <p><b>Location:</b> ${feature.properties.place}</p> 
+                      <p><b>Time:</b> ${time}</p>
+                      <p><b>Magnitude:</b> ${magnitude}</p>
+                      <p><b>Depth:</b> ${depth}</p>
+        `);
+
+        markers.addLayer(popup); // Add the marker to the feature group
+    });
+
+    // Adding the feature group to the map
+    myMap.addLayer(markers);
+
+    // Create a legend control
+var legend = L.control({ position: "bottomright" });
+
+// Define the onAdd method for the legend control
+legend.onAdd = function (map) {
+  // Create a div element for the legend
+  var div = L.DomUtil.create("div", "legend");
+
+  // Define the labels and corresponding colors for the legend
+  var labels = ['Depth <= 10', '10 < Depth <= 30', '30 < Depth <= 50', '50 < Depth <= 70', '70 < Depth <= 90', '90 < Depth <= 110', 'Depth > 110'];
+  var colors = ['blue', 'grey', 'green', 'yellow', 'red', 'purple', 'black'];
+
+  // Loop through the labels and colors and create the legend items
+  for (var i = 0; i < labels.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' + colors[i] + '"></i> ' + labels[i] + '<br>';
+  }
+
+  return div;
+};
+
+// Add the legend to the map
+legend.addTo(myMap);
+
+});
